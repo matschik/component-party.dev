897f519f7e4b8385d02d0a2f3660e5e0495c57fa
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-04-11 09:58:00 +0000

    fix precommit hook

--- src/tree.js
+++ src/tree.js
@@ -1,110 +1,110 @@
 export default [
-	{
-		id: 'reactivity',
-		title: 'Reactivity',
-		sections: [
-			{
-				id: 'declare-state',
-				title: 'Declare state',
-			},
-			{
-				id: 'update-state',
-				title: 'Update state',
-			},
-			{
-				id: 'computed-state',
-				title: 'Computed state',
-			},
-		],
-	},
-	{
-		id: 'templating',
-		title: 'Templating',
-		sections: [
-			{
-				id: 'minimal-template',
-				title: 'Minimal template',
-			},
-			{
-				id: 'styling',
-				title: 'Styling',
-			},
-			{
-				id: 'loop',
-				title: 'Loop',
-			},
-			{
-				id: 'event-click',
-				title: 'Event click',
-			},
-			{
-				id: 'dom-ref',
-				title: 'Dom ref',
-			},
-			{
-				id: 'conditional',
-				title: 'Conditional',
-			},
-			{
-				id: 'render',
-				title: 'Render',
-			},
-		],
-	},
-	{
-		id: 'lifecycle',
-		title: 'Lifecycle',
-		sections: [
-			{
-				id: 'on-mount',
-				title: 'On mount',
-			},
-			{
-				id: 'on-unmount',
-				title: 'On unmount',
-			},
-		],
-	},
-	{
-		id: 'component-composition',
-		title: 'Component composition',
-		sections: [
-			{
-				id: 'props',
-				title: 'Props',
-			},
-		],
-	},
-	{
-		id: 'form-input',
-		title: 'Form input',
-		sections: [
-			{
-				id: 'input-text',
-				title: 'Input text',
-			},
-			{
-				id: 'checkbox',
-				title: 'Checkbox',
-			},
-			{
-				id: 'radio',
-				title: 'Radio',
-			},
-			{
-				id: 'select',
-				title: 'Select',
-			},
-		],
-	},
-	{
-		id: 'webapp-features',
-		title: 'Webapp features',
-		sections: [
-			{
-				id: 'routing',
-				title: 'Routing',
-			},
-		],
-	},
-];
+  {
+    "id": "reactivity",
+    "title": "Reactivity",
+    "sections": [
+      {
+        "id": "declare-state",
+        "title": "Declare state"
+      },
+      {
+        "id": "update-state",
+        "title": "Update state"
+      },
+      {
+        "id": "computed-state",
+        "title": "Computed state"
+      }
+    ]
+  },
+  {
+    "id": "templating",
+    "title": "Templating",
+    "sections": [
+      {
+        "id": "minimal-template",
+        "title": "Minimal template"
+      },
+      {
+        "id": "styling",
+        "title": "Styling"
+      },
+      {
+        "id": "loop",
+        "title": "Loop"
+      },
+      {
+        "id": "event-click",
+        "title": "Event click"
+      },
+      {
+        "id": "dom-ref",
+        "title": "Dom ref"
+      },
+      {
+        "id": "conditional",
+        "title": "Conditional"
+      },
+      {
+        "id": "render",
+        "title": "Render"
+      }
+    ]
+  },
+  {
+    "id": "lifecycle",
+    "title": "Lifecycle",
+    "sections": [
+      {
+        "id": "on-mount",
+        "title": "On mount"
+      },
+      {
+        "id": "on-unmount",
+        "title": "On unmount"
+      }
+    ]
+  },
+  {
+    "id": "component-composition",
+    "title": "Component composition",
+    "sections": [
+      {
+        "id": "props",
+        "title": "Props"
+      }
+    ]
+  },
+  {
+    "id": "form-input",
+    "title": "Form input",
+    "sections": [
+      {
+        "id": "input-text",
+        "title": "Input text"
+      },
+      {
+        "id": "checkbox",
+        "title": "Checkbox"
+      },
+      {
+        "id": "radio",
+        "title": "Radio"
+      },
+      {
+        "id": "select",
+        "title": "Select"
+      }
+    ]
+  },
+  {
+    "id": "webapp-features",
+    "title": "Webapp features",
+    "sections": [
+      {
+        "id": "routing",
+        "title": "Routing"
+      }
+    ]
+  }
+]
