4ed33fd3c3ac4ca63a51dfa8b02a8fc4e486fe51
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-04-19 12:16:10 +0200

    content: add slot fallback

---
+++ content/4-component-composition/3-slot-fallback/react/FunnyButton.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export default function FunnyButton({ children }) {
+	return (
+		<button
+			style={{
+				background: 'rgba(0, 0, 0, 0.4)',
+				color: '#fff',
+				padding: '10px 20px',
+				fontSize: '30px',
+				border: '2px solid #fff',
+				margin: '8px',
+				transform: 'scale(0.9)',
+				boxShadow: '4px 4px rgba(0, 0, 0, 0.4)',
+				transition: 'transform 0.2s cubic-bezier(0.34, 1.65, 0.88, 0.925) 0s',
+				outline: '0',
+			}}
+		>
+			{children || <span>No content found</span>}
+		</button>
+	);
+}
+
+FunnyButton.propTypes = {
+	children: PropTypes.node,
+};
---
+++ content/4-component-composition/3-slot-fallback/vue3/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import FunnyButton from './FunnyButton.vue';
+</script>
+
+<template>
+	<FunnyButton />
+	<FunnyButton> I got content ! </FunnyButton>
+</template>
