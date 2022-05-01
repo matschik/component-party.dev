db8c158b4452aa2aef3fe4dd63b380b2b73b2165
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-05-01 02:15:43 +0200

    refactor: style redesign

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		extend: {
+		  fontFamily: {
+			sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+		  },
+		},
+	  },
 	plugins: [require('@tailwindcss/typography')],
 };
