e6cc28e2b097ab0188a475ab1a56131bbe3c544a
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-05-03 23:14:42 +0200

    fix(angular): angular files display

--- content/4-component-composition/1-props/angular/userprofile.component.ts
+++ content/4-component-composition/1-props/angular/userprofile.component.ts
@@ -1,21 +1,20 @@
 import { Component, Input } from '@angular/core';
 
-interface UserprofileComponentProps{
-  name:string,
-  age:number,
-  favouriteColors:string[],
-  isAvailable:boolean 
+interface UserprofileComponentProps {
+	name: string;
+	age: number;
+	favouriteColors: string[];
+	isAvailable: boolean;
 }
 
 @Component({
-  selector: 'app-userprofile',
-  templateUrl: './userprofile.component.html',
-  styleUrls: ['./userprofile.component.css']
+	selector: 'app-userprofile',
+	templateUrl: './userprofile.component.html',
+	styleUrls: ['./userprofile.component.css'],
 })
-
 export class UserprofileComponent implements UserprofileComponentProps {
-  @Input() name:string = "";
-  @Input() age:number = 0;
-  @Input() favouriteColors:string[] = []
-  @Input() isAvailable:boolean = false
+	@Input() name: string = '';
+	@Input() age: number = 0;
+	@Input() favouriteColors: string[] = [];
+	@Input() isAvailable: boolean = false;
 }
