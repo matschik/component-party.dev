df718c9c582d6214e5c93c3326ba674f5e3d63ed
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-05-04 18:21:19 +0200

    fix(content): remove React useMemo on computed state

--- content/1-reactivity/3-computed-state/react/DoubleCount.jsx
+++ content/1-reactivity/3-computed-state/react/DoubleCount.jsx
@@ -1,7 +1,7 @@
-import { useState, useMemo } from 'react';
+import { useState } from 'react';
 
 export default function DoubleCount() {
 	const [count] = useState(10);
-	const doubleCount = useMemo(() => count * 2, [count]);
+	const doubleCount = count * 2;
 	return <div>{doubleCount}</div>;
 }
