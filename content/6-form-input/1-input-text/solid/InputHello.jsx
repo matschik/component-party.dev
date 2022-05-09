f4fb222c3d32f4f3dd63f6af2f9e345754ea5dd8
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-05-09 16:25:40 +0200

    feat(form-input): add solid input-text
    Add input-text example for solidjs

---
+++ content/6-form-input/1-input-text/solid/InputHello.jsx
@@ -0,0 +1,16 @@
+import { createSignal } from "solid-js";
+
+export default function InputHello() {
+	const [text, setText] = createSignal('Hello world');
+
+	function handleChange(event) {
+		setText(event.target.value);
+	}
+
+	return (
+		<>
+			<p>{text}</p>
+			<input value={text()} onInput={handleChange} />
+		</>
+	);
+}
