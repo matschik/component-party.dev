0d995b8db0abba161e712a5f6d5b58199dee0db5
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-05-09 16:41:53 +0200

    feat(form-input): add solid select example

---
+++ content/6-form-input/4-select/solid/ColorSelect.jsx
@@ -0,0 +1,29 @@
+import { render, For } from "solid-js/web";
+import { createSignal } from "solid-js";
+
+const colors = [
+  { id: 1, text: "red" },
+  { id: 2, text: "blue" },
+  { id: 3, text: "green" },
+  { id: 4, text: "gray", isDisabled: true },
+];
+
+export default function ColorSelect() {
+  const [selectedColorId, setSelectedColorId] = createSignal(2);
+
+  function handleChange(event) {
+    setSelectedColorId(event.target.value);
+  }
+
+  return (
+    <select value={selectedColorId()} onChange={handleChange}>
+      <For each={colors}>
+        {color => (
+          <option value={color.id} disabled={color.isDisabled}>
+            {color.text}
+          </option>
+        )}
+      </For>
+    </select>
+  );
+}
