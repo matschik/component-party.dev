5bc8dd2399d0442d24b668c073df75e44c58702b
P Christopher Bowers <41601975+pcbowers@users.noreply.github.com>
2022-05-10 18:01:13 -0400

    fix: :bug: Updated Function Name
    Updated function name. Previously was entitled vue3 instead of alpine.

--- src/utils/createAlpineREPL.js
+++ src/utils/createAlpineREPL.js
@@ -1,6 +1,6 @@
 import { getParameters } from 'codesandbox/lib/api/define';
 
-export default function createVue3REPL() {
+export default function createAlpineREPL() {
 	const BASE_URL = 'https://codesandbox.io/api/v1/sandboxes/define?embed=1&parameters=';
 	const BASE_PREFIX = `<!DOCTYPE html>\n<html lang="en">\n  <head>\n    <meta charset="UTF-8" />\n    <meta name="viewport" content="width=device-width, initial-scale=1.0" />\n    <meta http-equiv="X-UA-Compatible" content="ie=edge" />\n    <title>Alpine.js Playground</title>\n    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>\n  </head>\n  <body>\n\n`;
 	const BASE_SUFFIX = `\n  </body>\n</html>`;
