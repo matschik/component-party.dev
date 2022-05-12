e65e1065fbd2485ef6270ca4ef23fdf4de4ec424
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-05-12 15:44:27 +0200

    feat: framework button toggle via alpine

--- src/components/scripts/alpine-init.js
+++ src/components/scripts/alpine-init.js
@@ -1,34 +1,37 @@
 import Alpine from 'alpinejs';
-//import FRAMEWORKS from '@/frameworks.mjs';
 
-const frameworksSelectedProxy = createLocaleStorageProxy('frameworks_display');
-
-// set default value of frameworksSelectedProxy
-if (!Object.values(frameworksSelectedProxy).length) {
-	const initialFrameworkIds = ['react', 'svelte'];
-	for (let i = 0; i < initialFrameworkIds.length; i++) {
-		frameworksSelectedProxy[i] = initialFrameworkIds[i];
-	}
-}
-
-Alpine.store('framework', {
-	selectedIds: frameworksSelectedProxy,
-	// get items(){
-	//     return this.selectedIds.map(fmwId => FRAMEWORKS.find((f) => f.id === fmwId))
-	// },
-	hide(fmwId) {
-		if (frameworksSelectedProxy.includes(fmwId)) {
-			const frameworkIndex = frameworksSelectedProxy.indexOf(fmwId);
-			delete frameworksSelectedProxy[frameworkIndex];
+Alpine.store('frameworksSelected', {
+	frameworksSelectedProxy: createLocaleStorageProxy('frameworks_display'),
+	selectedIds: [],
+	init() {
+		// set default value of frameworksSelectedProxy
+		if (!Object.values(this.frameworksSelectedProxy).length) {
+			const initialFrameworkIds = ['react', 'svelte'];
+			for (let i = 0; i < initialFrameworkIds.length; i++) {
+				this.frameworksSelectedProxy[i] = initialFrameworkIds[i];
+			}
 		}
-		this.selectedIds = [...frameworksSelectedProxy];
+		this.selectedIds = [...this.frameworksSelectedProxy];
+	},
+	has(fmwId) {
+		return this.selectedIds.includes(fmwId);
+	},
+	toggle(fmwId) {
+		this.has(fmwId) ? this.hide(fmwId) : this.show(fmwId);
+	},
+	hide(fmwId) {
+		if (this.has(fmwId)) {
+			const frameworkIndex = this.frameworksSelectedProxy.indexOf(fmwId);
+			delete this.frameworksSelectedProxy[frameworkIndex];
+		}
+		this.selectedIds = [...this.frameworksSelectedProxy];
 	},
 	show(fmwId) {
-		if (!frameworksSelectedProxy.includes(fmwId)) {
-			frameworksSelectedProxy.push(fmwId);
+		if (!this.has(fmwId)) {
+			this.frameworksSelectedProxy.push(fmwId);
 		}
 
-		this.selectedIds = [...frameworksSelectedProxy];
+		this.selectedIds = [...this.frameworksSelectedProxy];
 	},
 });
 Alpine.start();
