86102eea5255b9b8767f82f372c9dc04044df9a9
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-15 15:38:30 +0200

    fix(lit): remove unused import

--- content/2-templating/1-minimal-template/lit/hello-world.js
+++ content/2-templating/1-minimal-template/lit/hello-world.js
@@ -1,5 +1,5 @@
 import { LitElement, html } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
+import { customElement } from 'lit/decorators.js';
 
 @customElement('hello-world')
 export class HelloWorld extends LitElement {
