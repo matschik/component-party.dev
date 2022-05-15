4c79c23cfd2e7892afe6719f46b45543b4878b96
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-15 15:43:00 +0200

    feat(lit): add loop example

---
+++ content/2-templating/3-loop/lit/colors-list.js
@@ -0,0 +1,15 @@
+import { LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('colors-list')
+export class ColorsList extends LitElement {
+	colors = ['red', 'green', 'blue'];
+
+	render() {
+		return html`
+			<ul>
+				${this.colors.map((color) => html`<li>${color}</li>`)}
+			</ul>
+		`;
+	}
+}
