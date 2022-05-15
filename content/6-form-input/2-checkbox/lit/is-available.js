3d501206db31ba4a6d68923de052838b06c579c7
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-15 17:23:25 +0200

    feat(lit): add checkbox example

---
+++ content/6-form-input/2-checkbox/lit/is-available.js
@@ -0,0 +1,19 @@
+import { LitElement, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+
+@customElement('is-available')
+export class IsAvailable extends LitElement {
+	@state()
+	isAvailable = false;
+
+	handleChange() {
+		this.isAvailable = !this.isAvailable;
+	}
+
+	render() {
+		return html`
+			<input id="is-available" type="checkbox" ?checked=${this.isAvailable} @change=${this.handleChange} />
+			<label for="is-available">Is available</label>
+		`;
+	}
+}
