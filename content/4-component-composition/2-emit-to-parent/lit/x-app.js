68fdd75a235b07becc1f24f6089153234d3e970e
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-15 17:06:45 +0200

    feat(lit): add emit to parent example

---
+++ content/4-component-composition/2-emit-to-parent/lit/x-app.js
@@ -0,0 +1,26 @@
+import { LitElement, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+
+import './answer-button';
+
+@customElement('x-app')
+export class XApp extends LitElement {
+	@state()
+	canCome = true;
+
+	onAnswerNo() {
+		this.canCome = false;
+	}
+
+	onAnswerYes() {
+		this.canCome = true;
+	}
+
+	render() {
+		return html`
+			<p>Can I come ?</p>
+			<answer-button @yes=${this.onAnswerYes} @no=${this.onAnswerNo}></answer-button>
+			<p style="font-size: 50px;">${this.canCome ? '😀' : '😥'}</p>
+		`;
+	}
+}
