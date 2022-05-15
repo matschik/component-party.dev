3f2fc6408a01a16aade6e42f648dd460bc3128ba
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-05-15 19:27:37 +0200

    feat(lit): add update state example

---
+++ content/1-reactivity/2-update-state/lit/name.js
@@ -0,0 +1,17 @@
+import { LitElement, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+
+@customElement('x-name')
+export class XName extends LitElement {
+	@state()
+	name = 'John';
+
+	constructor() {
+		super();
+		this.name = 'Jane';
+	}
+
+	render() {
+		return html`<h1>Hello ${this.name}!</h1>`;
+	}
+}
