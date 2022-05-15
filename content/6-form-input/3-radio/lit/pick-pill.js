c1892fc978882609cf203a12862285196e5bfe62
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-15 17:28:04 +0200

    feat(lit): add radio example

---
+++ content/6-form-input/3-radio/lit/pick-pill.js
@@ -0,0 +1,24 @@
+import { LitElement, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+
+@customElement('pick-pill')
+export class PickPill extends LitElement {
+	@state()
+	picked = 'red';
+
+	handleChange(event) {
+		this.picked = event.target.value;
+	}
+
+	render() {
+		return html`
+			<div>Picked: ${this.picked}</div>
+
+			<input id="blue-pill" name="pill" ?checked=${this.picked === 'blue'} type="radio" value="blue" @change=${this.handleChange} />
+			<label for="blue-pill">Blue pill</label>
+
+			<input id="red-pill" name="pill" ?checked=${this.picked === 'red'} type="radio" value="red" @change=${this.handleChange} />
+			<label for="red-pill">Red pill</label>
+		`;
+	}
+}
