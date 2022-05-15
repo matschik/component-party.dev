060b03d2303bfa90910a174f41750be70d66a070
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-15 15:31:51 +0200

    feat(lit): add computed state example

---
+++ content/1-reactivity/3-computed-state/lit/double-count.js
@@ -0,0 +1,16 @@
+import { LitElement, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+
+@customElement('double-count')
+export class DoubleCount extends LitElement {
+	@state()
+	count = 10;
+
+	get doubleCount() {
+		return this.count * 2;
+	}
+
+	render() {
+		return html`<div>${this.doubleCount}</div>`;
+	}
+}
