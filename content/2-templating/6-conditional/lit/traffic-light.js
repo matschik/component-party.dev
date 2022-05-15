ba7b46459dbe0feff0d8e00cd0d596e99a02ec32
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-15 16:41:10 +0200

    feat(lit): add conditional example

---
+++ content/2-templating/6-conditional/lit/traffic-light.js
@@ -0,0 +1,38 @@
+import { LitElement, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+import { choose } from 'lit/directives/choose.js';
+
+const TRAFFIC_LIGHTS = ['red', 'orange', 'green'];
+
+@customElement('traffic-light')
+export class TrafficLight extends LitElement {
+	@state()
+	lightIndex = 0;
+
+	get light() {
+		return TRAFFIC_LIGHTS[this.lightIndex];
+	}
+
+	nextLight() {
+		if (this.lightIndex + 1 > TRAFFIC_LIGHTS.length - 1) {
+			this.lightIndex = 0;
+		} else {
+			this.lightIndex = this.lightIndex + 1;
+		}
+	}
+
+	render() {
+		return html`
+			<button @click=${this.nextLight}>Next light</button>
+			<p>Light is ${this.light}</p>
+			<p>
+				You must
+				${choose(this.light, [
+					['red', () => html`<span>STOP</span>`],
+					['orange', () => html`<span>SLOW DOWN</span>`],
+					['green', () => html`<span>GO</span>`],
+				])}
+			</p>
+		`;
+	}
+}
