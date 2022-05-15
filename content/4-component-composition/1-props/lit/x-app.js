1f805f59c8cb07e067ce96030252fe154a879277
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-15 16:59:36 +0200

    feat(lit): add props example

---
+++ content/4-component-composition/1-props/lit/x-app.js
@@ -0,0 +1,11 @@
+import { LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+import './user-profile';
+
+@customElement('x-app')
+export class XApp extends LitElement {
+	render() {
+		return html`<user-profile name="John" age="20" .favouriteColors=${['green', 'blue', 'red']} isavailable></user-profile>`;
+	}
+}
