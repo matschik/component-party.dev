dbc250b893222a748c1f82590543169557939de8
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-15 16:32:53 -0400

    ember: 3-radio

---
+++ content/6-form-input/3-radio/ember/pick-pill.js
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class PickPill extends Component {
+  @tracked picked = 'red';
+
+  handleChange = (event) => this.picked = event.target.value;
+}
