6d4cda13e546b290e31b8a327019d3654dbc7e01
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-15 16:45:08 +0200

    feat(lit): add on mount example

---
+++ content/3-lifecycle/1-on-mount/lit/page-title.js
@@ -0,0 +1,17 @@
+import { LitElement, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+
+@customElement('page-title')
+export class PageTitle extends LitElement {
+	@state()
+	pageTitle = '';
+
+	connectedCallback() {
+		super.connectedCallback();
+		this.pageTitle = document.title;
+	}
+
+	render() {
+		return html`<p>Page title: ${this.pageTitle}</p>`;
+	}
+}
