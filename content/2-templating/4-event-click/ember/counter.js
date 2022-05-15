b677f6b0335a48d23e30bf2bd55c3cc0a1e93102
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-15 15:07:33 -0400

    ember: 4-event-click

---
+++ content/2-templating/4-event-click/ember/counter.js
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class Counter extends Component {
+  @tracked count = 0;
+
+  incrementCount = () => this.count++;
+}
