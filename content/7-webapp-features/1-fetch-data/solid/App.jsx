cf8669851356aa0508d85e3178f3183f53c5a223
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-21 17:47:49 +0200

    feat(solid): add data fetch example (#84)
    * feat(solid): add data fetch example
    
    * fix(solid): update data fetch example

---
+++ content/7-webapp-features/1-fetch-data/solid/App.jsx
@@ -0,0 +1,35 @@
+import { createResource, For, Switch, Match } from 'solid-js';
+
+async function fetchUsers() {
+	return (await fetch('https://randomuser.me/api/?results=3')).json();
+}
+
+export default function App() {
+	const [data] = createResource(fetchUsers);
+	const users = () => data()?.results;
+
+	return (
+		<Switch>
+			<Match when={data.loading}>
+				<p>Fetching users...</p>
+			</Match>
+			<Match when={data.error}>
+				<p>An error occurred while fetchint users</p>
+			</Match>
+			<Match when={users()}>
+				<ul>
+					<For each={users()}>
+						{(user) => (
+							<li>
+								<img src={user.picture.thumbnail} alt="user" />
+								<p>
+									{user.name.first} {user.name.last}
+								</p>
+							</li>
+						)}
+					</For>
+				</ul>
+			</Match>
+		</Switch>
+	);
+}
