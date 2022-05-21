65137dd67da7a1e3ecd41704eec5eb939608157f
JosepBR <josep.bergay@gmail.com>
2022-05-21 17:46:00 +0200

    Add fetch data for vue3 (#82)
    * feat(content): add fetch data for vue3
    
    * fix(content): change vue3 fetch data to match React/Svelte examples

---
+++ content/7-webapp-features/1-fetch-data/vue3/useFetchUsers.js
@@ -0,0 +1,24 @@
+import { ref } from 'vue';
+
+export default function useFetchUsers() {
+	const data = ref();
+	const error = ref();
+	const isLoading = ref(false);
+
+	async function fetchData() {
+		isLoading.value = true;
+		try {
+			const response = await fetch('https://randomuser.me/api/?results=3');
+			const { results: users } = await response.json();
+			data.value = users;
+			error.value = undefined;
+		} catch (err) {
+			data.value = undefined;
+			error.value = err;
+		}
+		isLoading.value = false;
+	}
+	fetchData();
+
+	return { isLoading, error, data };
+}
