a73c2da12afb448dc16e271d33cb8a896847e7be
Beaury <Sebastien.Beaury@ca-tjrxqrq9lh.ht.home>
2022-05-22 16:21:33 -0400

    feat: add qwik framework section for declaring state

---
+++ content/1-reactivity/1-declare-state/qwik/Name.tsx
@@ -0,0 +1,7 @@
+import { component$, useStore } from '@builder.io/qwik';
+
+export const Name = component$(() => {
+	const store = useStore({ name: 'John' });
+
+	return <h1>Hello {store.name}</h1>;
+});
