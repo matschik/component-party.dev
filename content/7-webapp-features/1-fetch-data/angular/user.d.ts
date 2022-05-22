cf6aa79edc0f9e3110fefe966c87ada0e385066c
Israel de la Barrera <isra.lora99@gmail.com>
2022-05-22 13:43:18 +0200

    feat(angular): Improvement in data fetching and updated readme (#86)
    * feat(angular): add data fetch example
    
    * feat(angular): Improvement in data fetch example
    
    * feat(angular): updated angular progress in readme

--- content/7-webapp-features/1-fetch-data/angular/user.d.ts
+++ content/7-webapp-features/1-fetch-data/angular/user.d.ts
@@ -1,56 +1,12 @@
 export interface User {
-    gender: string;
-    name: {
-      title: string;
-      first: string;
-      last:  string;
-    };
-    location: {
-      street: {
-        number: number;
-        name:   string;
-      };
-      city:        string;
-      state:       string;
-      country:     string;
-      postcode:    number;
-      coordinates: {
-        latitude:  string;
-        longitude: string;
-      };
-      timezone:    {
-        offset:      string;
-        description: string;
-      };
-    };
-    email: string;
-    login: {
-      uuid:     string;
-      username: string;
-      password: string;
-      salt:     string;
-      md5:      string;
-      sha1:     string;
-      sha256:   string;
-    };
-    dob: {
-      date: Date;
-      age:  number;
-    };
-    registered: {
-      date: Date;
-      age:  number;
-    };
-    phone: string;
-    cell: string;
-    id: {
-      name:  string;
-      value: string;
-    };
-    picture: {
-      large:     string;
-    medium:    string;
-    thumbnail: string;
-    };
-    nat: string;
-  }
+  name: {
+    title: string;
+    first: string;
+    last:  string;
+  };
+  picture: {
+    large:     string;
+  medium:    string;
+  thumbnail: string;
+  };
+}
