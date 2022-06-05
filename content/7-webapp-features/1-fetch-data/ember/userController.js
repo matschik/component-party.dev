d9df7f24f0d5a5031d277394080d295757012121
Mehul Kiran Chaudhari <chaudharimehul2001@gmail.com>
2022-06-05 15:28:20 +0530

    added: required changes

--- content/7-webapp-features/1-fetch-data/ember/userController.js
+++
@@ -1,5 +0,0 @@
-import Controller from '@ember/controller';
-
-export default class UserController extends Controller {
-	@tracked allUsers = this.model;
-}
--- content/7-webapp-features/1-fetch-data/ember/users.js
+++ content/7-webapp-features/1-fetch-data/ember/users.js
@@ -1,19 +1,17 @@
 import Route from '@ember/routing/route';
-import fetch from 'fetch';
 
 export default class UsersRoute extends Route {
-	model = async () => {
+	async model() {
+		let users, error;
+		let isLoading = true;
 		try {
 			const response = await fetch('https://randomuser.me/api/?results=3');
-			if (!response.ok) {
-				if (response.status == 401) {
-					throw new Error('Please try again');
-				}
-				throw new Error('Oops, We ran into a problem!');
-			}
-			return await response.json();
-		} catch (error) {
-			console.error(error);
+			users = await response.json();
+			isLoading = false;
+		} catch (err) {
+			error = err;
+			isLoading = false;
 		}
-	};
+		return { isLoading, users, error };
+	}
 }
