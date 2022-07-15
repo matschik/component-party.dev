a191bf26894488b1fd40cb1bb0bb66e280d94912
Mohamed Ahmed Fouad <mido.goodboy@gmail.com>
2022-07-15 15:58:34 +0200

    Remove the export and fix the formatting

--- content/2-templating/2-styling/qwik/CssStyle.tsx
+++ content/2-templating/2-styling/qwik/CssStyle.tsx
@@ -1,17 +1,15 @@
 import { component$, Host, useStyles$ } from '@builder.io/qwik';
 
-export const AppCss = `
-.title {
-	color: red;
-}
-`;
-
-export const CssStyle = component$(() => {
-  useStyles$(AppCss);
+export const App = component$(() => {
+  useStyles$(`
+    .title {
+      color: red;
+    }
+    `);
   return (
     <Host>
-    	<h1 class="title">I am red</h1>
-		  <button style={{ 'font-size': '10rem' }}>I am a button</button>
+      <h1 class="title">I am red</h1>
+      <button style={{ 'font-size': '10rem' }}>I am a button</button>
     </Host>
   );
 });
