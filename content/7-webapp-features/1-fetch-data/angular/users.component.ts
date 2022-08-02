1a6c365fa12d68cb8fb4a652867e55df70849be2
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-08-03 00:29:53 +0200

    Update users.component.ts

--- content/7-webapp-features/1-fetch-data/angular/users.component.ts
+++ content/7-webapp-features/1-fetch-data/angular/users.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { User } from './user';
-import { UserService } from './user.service';
+import { User, UserService } from './user.service';
 
 @Component({
 	selector: 'app-users',
