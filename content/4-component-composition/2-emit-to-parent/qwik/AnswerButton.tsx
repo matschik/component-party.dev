d7f226dd089ca72f6133d2e4cc087b7e6d82bb6b
Casper Engelmann <me@casperengelmann.com>
2022-08-27 23:40:29 +0200

    feat(qwik): add component-composition/emit-to-parent (#114)
    * fix(qwik): typescript eslint parser
    
    * feat(qwik): add answer button example

---
+++ content/4-component-composition/2-emit-to-parent/qwik/AnswerButton.tsx
@@ -0,0 +1,18 @@
+import { component$, PropFunction } from '@builder.io/qwik';
+
+type Props = {
+	onYes$: PropFunction<() => void>;
+	onNo$: PropFunction<() => void>;
+};
+
+const AnswerButton = component$((props: Props) => {
+	return (
+		<>
+			<button onClick$={props.onYes$}>YES</button>
+
+			<button onClick$={props.onNo$}>NO</button>
+		</>
+	);
+});
+
+export default AnswerButton;
