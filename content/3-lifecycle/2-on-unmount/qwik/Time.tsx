2bd65771cddadb2dec029affe042bf99bf42000c
Casper Engelmann <me@casperengelmann.com>
2022-08-27 23:42:16 +0200

    feat(qwik): add lifecycle/on-unmount (#112)
    * feat(qwik): add time example
    
    * fix(qwik): typescript eslint parser

---
+++ content/3-lifecycle/2-on-unmount/qwik/Time.tsx
@@ -0,0 +1,17 @@
+import { component$, useClientEffect$, useStore } from '@builder.io/qwik';
+
+export const App = component$(() => {
+	const store = useStore({
+		time: new Date().toLocaleTimeString(),
+	});
+
+	useClientEffect$(() => {
+		const timer = setInterval(() => {
+			store.time = new Date().toLocaleTimeString();
+		}, 1000);
+
+		return () => clearInterval(timer);
+	});
+
+	return <p>Current time: {store.time}</p>;
+});
