056cc437ba1a1319536f2ea9533620280b93a742
Casper Engelmann <me@casperengelmann.com>
2022-08-27 23:41:29 +0200

    feat(qwik): add component-composition/props (#113)
    * fix(qwik): typescript eslint parser
    
    * feat(qwik): add user profile example

---
+++ content/4-component-composition/1-props/qwik/App.tsx
@@ -0,0 +1,8 @@
+import { component$ } from '@builder.io/qwik';
+import UserProfile from './UserProfile';
+
+const App = component$(() => {
+	return <UserProfile name="John" age={20} favouriteColors={['green', 'blue', 'red']} isAvailable />;
+});
+
+export default App;
