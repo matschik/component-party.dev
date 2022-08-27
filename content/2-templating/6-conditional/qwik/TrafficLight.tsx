d70ccecc01d2f71c85b39ce6ad95b3a4b48e46de
Casper Engelmann <me@casperengelmann.com>
2022-08-27 13:16:57 +0200

    feat(qwik): add traffic light example

---
+++ content/2-templating/6-conditional/qwik/TrafficLight.tsx
@@ -0,0 +1,32 @@
+import { $, component$, useStore } from '@builder.io/qwik';
+
+export const TRAFFIC_LIGHTS = ['red', 'orange', 'green'];
+
+export const App = component$(() => {
+	const store = useStore({
+		lightIndex: 0,
+	});
+
+	const light = TRAFFIC_LIGHTS[store.lightIndex];
+
+	const nextLight = $(() => {
+		if (store.lightIndex + 1 > TRAFFIC_LIGHTS.length - 1) {
+			store.lightIndex = 0;
+		} else {
+			store.lightIndex += 1;
+		}
+	});
+
+	return (
+		<>
+			<button onClick$={nextLight}>Next light</button>
+			<p>Light is: {light}</p>
+			<p>
+				You must
+				{light === 'red' && <span>STOP</span>}
+				{light === 'orange' && <span>SLOW DOWN</span>}
+				{light === 'green' && <span>GO</span>}
+			</p>
+		</>
+	);
+});
