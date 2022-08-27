1cab70670c52dd0e4972618f8cb31ebaf2fe0f19
Casper Engelmann <me@casperengelmann.com>
2022-08-27 13:24:26 +0200

    feat(qwik): add user profile example

---
+++ content/4-component-composition/1-props/qwik/UserProfile.tsx
@@ -0,0 +1,23 @@
+import { component$ } from '@builder.io/qwik';
+
+type Props = {
+	name: string;
+	age: number;
+	favouriteColors: string[];
+	isAvailable: boolean;
+};
+
+const UserProfile = component$((props: Props) => {
+	const { name = '', age = null, favouriteColors = [], isAvailable = false } = props;
+
+	return (
+		<>
+			<p>My name is {name} !</p>
+			<p>My age is {age} !</p>
+			<p>My favourite colors are {favouriteColors.join(', ')} !</p>
+			<p>I am {isAvailable ? 'available' : 'not available'}</p>
+		</>
+	);
+});
+
+export default UserProfile;
