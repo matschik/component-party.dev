9501b90cf77853af3298cfc85eb0d425067a093a
Casper Engelmann <me@casperengelmann.com>
2022-08-27 23:44:01 +0200

    feat(qwik): add lifecycle/on-mount (#111)
    * feat(qwik): add page title example
    
    * fix(qwik): typescript eslint parser

---
+++ content/3-lifecycle/1-on-mount/qwik/PageTitle.tsx
@@ -0,0 +1,13 @@
+import { component$, useClientEffect$, useStore } from '@builder.io/qwik';
+
+export const App = component$(() => {
+	const store = useStore({
+		pageTitle: '',
+	});
+
+	useClientEffect$(() => {
+		store.pageTitle = document.title;
+	});
+
+	return <p>Page title: {store.pageTitle}</p>;
+});
