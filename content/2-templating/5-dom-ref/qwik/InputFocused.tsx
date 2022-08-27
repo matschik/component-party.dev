d009e1d4f5fc0ac5deb38b3336a128f0970f5af4
Casper Engelmann <me@casperengelmann.com>
2022-08-27 23:43:34 +0200

    feat(qwik): add templating/dom-ref (#109)
    * feat(qwik): add input focused example
    
    * fix(qwik): typescript eslint parser

---
+++ content/2-templating/5-dom-ref/qwik/InputFocused.tsx
@@ -0,0 +1,9 @@
+import { component$, useClientEffect$, useRef } from '@builder.io/qwik';
+
+export const InputFocused = component$(() => {
+	const inputElement = useRef(null);
+
+	useClientEffect$(() => inputElement.current.focus());
+
+	return <input type="text" ref={inputElement} />;
+});
