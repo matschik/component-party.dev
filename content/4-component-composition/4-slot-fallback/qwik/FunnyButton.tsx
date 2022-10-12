bd8b630ad93cdf6f02174049d9e00e3896c5c334
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-10-13 00:55:15 +0200

    feat(qwik): add webapp features examples

--- content/4-component-composition/4-slot-fallback/qwik/FunnyButton.tsx
+++ content/4-component-composition/4-slot-fallback/qwik/FunnyButton.tsx
@@ -5,26 +5,26 @@ import { component$, Slot, useStylesScoped$ } from '@builder.io/qwik';
  * with no immediate plans on adding support again.
  * https://github.com/BuilderIO/qwik/issues/1106
  *
- * The recommended fallback is something like this using CSS
+ * The recommended workaround is something like this, using CSS
  */
 const FunnyButton = component$(() => {
 	useStylesScoped$(`
-    button {
-      background: rgba(0, 0, 0, 0.4);
-      color: #fff;
-      padding: 10px 20px;
-      font-size: 30px;
-      border: 2px solid #fff;
-      margin: 8px;
-      transform: scale(0.9);
-      box-shadow: 4px 4px rgba(0, 0, 0, 0.4);
-      transition: transform 0.2s cubic-bezier(0.34, 1.65, 0.88, 0.925) 0s;
-      outline: 0;
-    }
+      button {
+        background: rgba(0, 0, 0, 0.4);
+        color: #fff;
+        padding: 10px 20px;
+        font-size: 30px;
+        border: 2px solid #fff;
+        margin: 8px;
+        transform: scale(0.9);
+        box-shadow: 4px 4px rgba(0, 0, 0, 0.4);
+        transition: transform 0.2s cubic-bezier(0.34, 1.65, 0.88, 0.925) 0s;
+        outline: 0;
+      }
 
-    .slot:not(:empty) + .fallback {
-      display: none;
-    }
+      .slot:not(:empty) + .fallback {
+        display: none;
+      }
   `);
 	return (
 		<button>
---
+++ content/7-webapp-features/1-fetch-data/qwik/App.tsx
@@ -0,0 +1,41 @@
+import { component$, useResource$, Resource } from '@builder.io/qwik';
+
+type UsersResponse = {
+	results: {
+		picture: {
+			thumbnail: string;
+		};
+		name: {
+			first: string;
+			last: string;
+		};
+	}[];
+};
+
+export async function fetchUsers() {
+	return (await fetch('https://randomuser.me/api/?results=3')).json();
+}
+
+export const App = component$(() => {
+	const data = useResource$<UsersResponse>(fetchUsers);
+
+	return (
+		<Resource
+			value={data}
+			onPending={() => <p>Fetching users...</p>}
+			onRejected={() => <p>An error occurred while fetching users</p>}
+			onResolved={({ results: users }) => (
+				<ul>
+					{users.map((user) => (
+						<li>
+							<img src={user.picture.thumbnail} alt="user" />
+							<p>
+								{user.name.first} {user.name.last}
+							</p>
+						</li>
+					))}
+				</ul>
+			)}
+		/>
+	);
+});
