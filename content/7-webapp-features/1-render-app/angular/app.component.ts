3bd9e308632ba6bf370fc93d29ddeabf7867c144
til0r <til0r@MBP-di-Antonio.wind3.hub>
2023-02-09 11:09:49 +0100

    Upadate render-app angular with comments on pull request

--- content/7-webapp-features/1-render-app/angular/app.component.ts
+++ content/7-webapp-features/1-render-app/angular/app.component.ts
@@ -3,10 +3,9 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "Component Party";
+  title = "Hello World";
 
   constructor() {}
 }
