a949e27814e46ab3712d4fb32348f27fad4bbaba
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2023-02-11 05:03:04 +0800

    Add Component composition > Context for vue2 (#153)

---
+++ content/4-component-composition/5-context/vue2/App.vue
@@ -0,0 +1,35 @@
+<script>
+import UserProfile from "./UserProfile.vue";
+
+export default {
+  components: { UserProfile },
+  provide() {
+    return {
+      user: Object.assign(this.user, {
+        updateUsername: this.updateUsername,
+      }),
+    };
+  },
+  data() {
+    return {
+      user: {
+        id: 1,
+        username: "unicorn42",
+        email: "unicorn42@example.com",
+      },
+    };
+  },
+  methods: {
+    updateUsername(newUsername) {
+      this.user.username = newUsername;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Welcome back, {{ user.username }}</h1>
+    <UserProfile />
+  </div>
+</template>
