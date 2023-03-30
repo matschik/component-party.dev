11975c888a99a4499d695365a92d1345d5edf55b
Brian Franco <mastastealth+git@gmail.com>
2023-03-29 23:09:55 -0400

    Ember "Context" Example (#160)
    * Ember "Context" Example
    
    * Forgot backing class

---
+++ content/4-component-composition/5-context/ember/user-profile.js
@@ -0,0 +1,10 @@
+import Component from '@glimmer/component';
+import { service } from '@ember/service';
+
+export default class UserProfileComponent extends Component {
+  @service userService;
+
+  get user() {
+    return this.userService.user;
+  }
+}
