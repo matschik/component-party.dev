5355bcd9818e022b44ad9090d4d7ae3f28f6e67c
Harald Brunner <brunnerh@users.noreply.github.com>
2023-07-21 21:14:02 +0200

    Restore functions.

--- content/4-component-composition/2-emit-to-parent/svelte/App.svelte
+++ content/4-component-composition/2-emit-to-parent/svelte/App.svelte
@@ -2,8 +2,16 @@
   import AnswerButton from "./AnswerButton.svelte";
 
   let canCome = true;
+
+  function onAnswerNo() {
+    canCome = false;
+  }
+
+  function onAnswerYes() {
+    canCome = true;
+  }
 </script>
 
 <p>Are you happy?</p>
-<AnswerButton on:yes={() => (canCome = true)} on:no={() => (canCome = false)} />
+<AnswerButton on:yes={onAnswerYes} on:no={onAnswerNo} />
 <p style="font-size: 50px;">{canCome ? "😀" : "😥"}</p>
