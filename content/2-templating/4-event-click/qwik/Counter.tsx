0688fd271636f96155c5291a74b46b488a971a8f
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2023-07-21 21:09:59 +0200

    chore(qwik): use function instead of inline function to match with other framework examples

--- content/2-templating/4-event-click/qwik/Counter.tsx
+++ content/2-templating/4-event-click/qwik/Counter.tsx
@@ -2,10 +2,15 @@ import { component$, useStore } from "@builder.io/qwik";
 
 export const Counter = component$(() => {
   const store = useStore({ count: 0 });
+  
+  const incrementCount = $(() => {
+    store.count++;
+  });
+
   return (
     <>
       <p>Counter: {store.count}</p>
-      <button onClick$={() => store.count++}>Increment</button>
+      <button onClick$={incrementCount}>Increment</button>
     </>
   );
 });
