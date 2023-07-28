19918ec618cc752324a42fe25b83a6ad0596ae13
Raghav Kanwal <raghav.kanwal@gmail.com>
2023-07-28 12:47:59 -0700

    Added AppRender snippet for Angular (#178)

---
+++ content/7-webapp-features/1-render-app/angular/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
