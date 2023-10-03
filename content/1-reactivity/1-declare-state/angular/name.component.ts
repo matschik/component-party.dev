8d2c2654d14d544432cf6daf129c39f0e2f2e0a6
Cássio Gabriel Santos <gabi04061997@gmail.com>
2023-10-03 11:36:18 -0300

    refactor: using angular signals

--- content/1-reactivity/1-declare-state/angular/name.component.ts
+++ content/1-reactivity/1-declare-state/angular/name.component.ts
@@ -1,9 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
   selector: "app-name",
   template: `<h1>Hello {{ name }}</h1>`,
 })
 export class NameComponent {
-  name = "John";
+  name = signal("John");
 }
