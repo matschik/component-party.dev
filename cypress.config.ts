002704cd25ba4935d2f6df35e02feb9246395d80
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2024-03-05 17:24:20 +0100

    test(*): add cypress and e2e tests

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:5173",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: "svelte",
+      bundler: "vite",
+    },
+  },
+});
---
+++ cypress/e2e/initial.cy.ts
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+describe("Initial frameworks", () => {
+  it("initial frameworks from scratch", () => {
+    cy.visit("/");
+    cy.get("[data-framework-id-selected-list]").should(
+      "have.attr",
+      "data-framework-id-selected-list",
+      "react,svelte4"
+    );
+  });
+
+  it("initial frameworks from local storage", () => {
+    cy.visit("/", {
+      onBeforeLoad(win) {
+        win.localStorage.setItem("framework_display", '["vue3","svelte4"]');
+      },
+    });
+    cy.get("[data-framework-id-selected-list]").should(
+      "have.attr",
+      "data-framework-id-selected-list",
+      "vue3,svelte4"
+    );
+  });
+});
+
+describe("pages", () => {
+  it("compare page", () => {
+    cy.visit("/compare/vue2-vs-vue3");
+    cy.get("[data-framework-id-selected-list]").should(
+      "have.attr",
+      "data-framework-id-selected-list",
+      "vue2,vue3"
+    );
+  });
+});
