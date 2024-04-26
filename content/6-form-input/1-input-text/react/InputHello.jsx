ad6a5046f71e78bb3addeb7f33e6f200b84f3a8a
Ahuing <guohuihot@163.com>
2024-04-26 11:42:15 +0800

    Update InputHello.jsx
    onInput

--- content/6-form-input/1-input-text/react/InputHello.jsx
+++ content/6-form-input/1-input-text/react/InputHello.jsx
@@ -10,7 +10,7 @@ export default function InputHello() {
   return (
     <>
       <p>{text}</p>
-      <input value={text} onChange={handleChange} />
+      <input value={text} onInput={handleChange} />
     </>
   );
 }
