af4440cb76f70111a75faed5fe8aab8112c6af00
Intza <huangyinhaow@outlook.com>
2024-06-05 19:43:52 +0800

    fix bugs

--- content/1-reactivity/1-declare-state/regularFramework/Name.ts
+++ content/1-reactivity/1-declare-state/regularFramework/Name.ts
@@ -2,6 +2,6 @@ import { NewElement, NewSignal } from "regular-framework/client";
 
 export default () => {
   const name = NewSignal("John");
-  name.value = "Jane";
+
   return NewElement("h1", {}, name);
 };
