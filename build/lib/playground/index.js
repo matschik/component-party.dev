522d669a0f0ab296972cb8c8098a91e21cab0f1e
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2024-10-25 09:35:08 +0200

    feat(playground): add svelte 5 playground (#270)
    * feat(playground): add svelte 5 playground
    
    * fix(playground): exclude svelte 5 examples that includes html files since it does not work
    
    * fix: use "/untitled" on Svelte 5 playground base url
    
    Co-authored-by: brunnerh <brunnerh@users.noreply.github.com>
    
    * feat(playground): include snippet title key in json hashed
    
    ---------
    
    Co-authored-by: brunnerh <brunnerh@users.noreply.github.com>

--- build/lib/playground/index.js
+++ build/lib/playground/index.js
@@ -1,13 +1,12 @@
 import createAlpinePlayground from "./createAlpinePlayground.js";
-// import createSveltePlayground from "./createSveltePlayground.js";
+import createSvelte5Playground from "./createSvelte5Playground.js";
 import createVue3Playground from "./createVue3Playground.js";
 import createSolidPlayground from "./createSolidPlayground.js";
 import createMarkoPlayground from "./createMarkoPlayground.js";
 
 export default {
   vue3: createVue3Playground(),
-  // @TODO: Svelte playground is broken, fixing it when possible
-  //svelte: createSveltePlayground(),
+  svelte5: createSvelte5Playground(),
   alpine: createAlpinePlayground(),
   solid: createSolidPlayground(),
   marko: createMarkoPlayground(),
