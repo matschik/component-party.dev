7ff6f039386880ec48a31cd106099c033632c8c5
yugo <yugo969@gmail.com>
2025-06-06 22:49:00 +0900

    feat(react-tsx): add slot example

---
+++ content/4-component-composition/3-slot/reactTsx/App.tsx
@@ -0,0 +1,5 @@
+import FunnyButton from "./FunnyButton.jsx";
+
+export default function App() {
+  return <FunnyButton>Click me!</FunnyButton>;
+}
---
+++ content/4-component-composition/3-slot/reactTsx/FunnyButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface FunnyButtonProps {
+  children: React.ReactNode;
+}
+
+export default function FunnyButton({ children }: FunnyButtonProps) {
+  return (
+    <button
+      style={{
+        background: "rgba(0, 0, 0, 0.4)",
+        color: "#fff",
+        padding: "10px 20px",
+        fontSize: "30px",
+        border: "2px solid #fff",
+        margin: "8px",
+        transform: "scale(0.9)",
+        boxShadow: "4px 4px rgba(0, 0, 0, 0.4)",
+        transition: "transform 0.2s cubic-bezier(0.34, 1.65, 0.88, 0.925) 0s",
+        outline: "0",
+      }}
+    >
+      <p>🤣</p>
+      {children}
+      <p>🤣</p>
+    </button>
+  );
+}
