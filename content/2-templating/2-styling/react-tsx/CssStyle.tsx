27d1ab602e0a5e71ecb8ef97bbe33d7f4d472890
yugo <yugo969@gmail.com>
2025-06-06 22:28:52 +0900

    feat(react-tsx): add styling example

---
+++ content/2-templating/2-styling/react-tsx/CssStyle.tsx
@@ -0,0 +1,10 @@
+import "./style.css";
+
+export default function CssStyle() {
+  return (
+    <>
+      <h1 className="title">I am red</h1>
+      <button style={{ fontSize: "10rem" }}>I am a button</button>
+    </>
+  );
+}
