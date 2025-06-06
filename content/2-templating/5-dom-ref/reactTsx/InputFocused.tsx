2dc25868ade77237512218badd08f80543f8d645
yugo <yugo969@gmail.com>
2025-06-06 22:43:03 +0900

    feat(react-tsx): add dom-ref example

---
+++ content/2-templating/5-dom-ref/reactTsx/InputFocused.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from "react";
+
+export default function InputFocused() {
+  const inputElement = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputElement.current?.focus();
+  }, []);
+
+  return <input type="text" ref={inputElement} />;
+}
