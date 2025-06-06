c4bf8ea4926620c73cee81bfd02ba24dbded4913
yugo <yugo969@gmail.com>
2025-06-06 22:39:22 +0900

    fix(react-tsx): rename framework id to reactTsx

--- content/1-reactivity/1-declare-state/react-tsx/Name.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export default function Name() {
-  const [name] = useState<string>("John");
-
-  return <h1>Hello {name}</h1>;
-}
--- content/2-templating/1-minimal-template/react-tsx/HelloWorld.tsx
+++
@@ -1,3 +0,0 @@
-export default function HelloWorld() {
-  return <h1>Hello world</h1>;
-}
