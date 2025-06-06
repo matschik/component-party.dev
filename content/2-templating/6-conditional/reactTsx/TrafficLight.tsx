31d80a6b4583e68596ec581fccd624240d3fde27
yugo <yugo969@gmail.com>
2025-06-06 22:43:35 +0900

    feat(react-tsx): add conditional example

---
+++ content/2-templating/6-conditional/reactTsx/TrafficLight.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const TRAFFIC_LIGHTS = ["red", "orange", "green"];
+
+export default function TrafficLight() {
+  const [lightIndex, setLightIndex] = useState<number>(0);
+
+  const light = TRAFFIC_LIGHTS[lightIndex];
+
+  function nextLight() {
+    setLightIndex((lightIndex + 1) % TRAFFIC_LIGHTS.length);
+  }
+
+  return (
+    <div>
+      <button onClick={nextLight}>Next light</button>
+      <p>Light is: {light}</p>
+      <p>
+        You must
+        {light === "red" && <span>STOP</span>}
+        {light === "orange" && <span>SLOW DOWN</span>}
+        {light === "green" && <span>GO</span>}
+      </p>
+    </div>
+  );
+}
